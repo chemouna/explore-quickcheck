275f6f76d2dd77731e309b532fa50884f27d7080
Mouna Cheikhna <cheikhnamouna@gmail.com>
2016-10-22 19:03:29 +0200

    generators for different elements.

--- src/qcCompileOptim.hs
+++ src/qcCompileOptim.hs
@@ -1,10 +1,11 @@
-
 module QcCompileOptim where
 
-import Control.Monad
-import Control.Monad.Trans.State.Lazy
-import Data.List
+import           Control.Monad
+import           Control.Monad.Trans.State.Lazy
+import           Data.List
 import qualified Data.Map as M
+import           Test.QuickCheck
+import           Test.QuickCheck.All
 
 data Expression =
       Var   Variable
@@ -14,7 +15,7 @@ data Expression =
     deriving (Eq, Ord)
 
 data Variable = V String deriving (Eq, Ord)
- 
+
 data Value =
       IntVal Int
     | BoolVal Bool
@@ -38,7 +39,7 @@ evalE (Var x)       = get >>= return . M.findWithDefault (IntVal 0) x
 evalE (Val v)       = return v
 evalE (Plus e1 e2)  = return (intOp (+) 0 IntVal) `ap` evalE e1 `ap` evalE e2
 evalE (Minus e1 e2) = return (intOp (-) 0 IntVal) `ap` evalE e1 `ap` evalE e2
- 
+
 evalS :: Statement -> State WState ()
 evalS w@(While e s)    = evalS (If e (Sequence s w) Skip)
 evalS Skip             = return ()
@@ -48,7 +49,7 @@ evalS (Assign x e )    = do v <- evalE e
                             put $ M.insert x v m
                             return ()
 evalS (If e s1 s2)     = do v <- evalE e
-                            case v of 
+                            case v of
                               BoolVal True  -> evalS s1
                               BoolVal False -> evalS s2
                               _             -> return ()
@@ -58,6 +59,20 @@ intOp op _ c (IntVal x) (IntVal y) = c $ x `op` y
 intOp _  d c _          _          = c d
 
 
+-- generator for variables
+instance Arbitrary Variable where
+  arbitrary = do x <- elements ['A'..'Z']
+                 return $ V [x]
 
+-- generator for values
+instance Arbitrary Value where
+  arbitrary = oneof [ liftM IntVal arbitrary, liftM BoolVal arbitrary]
 
+-- generator for expressions
+instance Arbitrary Expression where
+  arbitrary = frequency [(1, liftM Var arbitrary),
+                         (1, liftM Val arbitrary),
+                         (5, liftM2 Plus arbitrary arbitrary),
+                         (5, liftM2 Minus arbitrary arbitrary)]
 
+-- generator for WState so we can run while for arbitrary input config
